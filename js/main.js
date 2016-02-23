0d406b845d7a9f0aeb8131d527d3cb484063c7f7
Manish Garg <manish.garg@paxcel.net>
2016-02-23 12:52:27 +0530

    changing slide on scroll

--- js/main.js
+++ js/main.js
@@ -36,9 +36,6 @@ $(function(){
         contentType: 'html',
         loopCount: false
     });
-    
-	
-	
 
     wow = new WOW(
       {
@@ -47,7 +44,6 @@ $(function(){
       }
     );
     wow.init();
-
 
 
     //Moving slides effect
@@ -65,16 +61,43 @@ $(function(){
         }
     })
     slidesInterval = setInterval(changeslideAuto,slidesIntervalTime)
+    
 	
-	
-$('#skuButton').on('click',function(){
+    $('#skuButton').on('click',function(){
 		var iframe = $("#priceCompareFrame");
         if(iframe.attr("src") !== iframe.data("src")){
             iframe.attr("src", iframe.data("src"));    
         }
-		
 	});
+    
+    $('#overview').on('DOMMouseScroll mousewheel', function ( event ) {
+        if(!isNavAtTop()){
+            return true;
+        }
+        if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
+            //scroll down
+            var slidesCount = $('.slides-heads').children().length
+            var activeHeadIndex = $('.slides-heads').find('.active-slide-head').index();
+            activeHeadIndex++;
+            if(activeHeadIndex >= slidesCount){
+                return true;
+            }
+            $('.slides-heads .slide-head').eq(activeHeadIndex).find('a').click();
+            resetslidesInterval();
+            //prevent page fom scrolling
+            return false;
+        } else {
+            //scroll up
+            return true;
+        }
+    });
 	
+    function isNavAtTop(elem){
+        var docViewTop = $(window).scrollTop();
+        var navTop = $('#navigation-bar').offset().top;
+        return (navTop == docViewTop);
+    }
+    
 });
 
 
