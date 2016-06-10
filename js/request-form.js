c79b1fe7b19fdaa0b9e427295574da0492d047b8
vivekpaxcel <vivek.kumar@paxcel.net>
2016-06-10 15:07:17 +0530

    form update

--- js/request-form.js
+++ js/request-form.js
@@ -1,18 +1,28 @@
 // JavaScript Document 
-function submitRequestForDemo(){
-$("#contactform").submit(function(e){
- 	var params= $(this).serializeArray();
-    var action= $(this).attr("action");
-     $.ajax({
-        url : action,
-        type: "POST",
-        data : params,
-        mimeType: "text/html; charset=utf-8",
-        success:function(data, textStatus, jqXHR)  {
-            alert("We will contact you shortly");
-        },error: function(jqXHR, textStatus, errorThrown)  {
-             console.log("error");
-        }
-    });
-	 e.preventDefault(); //STOP default action
-}); }
+submitForm = function(form_id) {
+ $(("#"+form_id)).submit(function(e)
+ {
+  var params= $(this).serializeArray();
+  var action= $(this).attr("action");
+   $.ajax({
+   url : action,
+   type: "POST",
+   data : params,
+   mimeType: "text/html; charset=utf-8",
+   success:function(data, textStatus, jqXHR)  {
+    //data: return data from server on success fully complete the rquest
+   },error: function(jqXHR, textStatus, errorThrown)  {
+     console.log("error");
+   }
+  });
+  e.preventDefault(); //STOP default action
+ });
+}
+
+$document.ready(function(){
+ 
+ submitForm("contactform");
+ submitForm("contactform2");
+ submitForm("contactform3");
+ 
+});
