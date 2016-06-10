871562d41b6e2c745895bcc13f429284528f8ec2
vivekpaxcel <vivek.kumar@paxcel.net>
2016-06-10 11:29:21 +0530

    update request-a-demo.html

---
+++ js/request-form.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+function submitRequestForDemo(){
+	
+	var data = new FormData();
+	data.append('_subject', 'REQUEST A DEMO');
+	data.append('mobile_number', '9899709711');
+	data.append('_replyto', 'sagararora19992@gmail.com');
+	data.append('description', 'Testing');
+	data.append('name', 'Testing');
+	
+	
+	
+	$.ajax({
+		type:'POST',
+		url:'http://kinator.paxcom.net/paxcom/demo',
+		data:data,	
+		processData: false,
+  		contentType: false,	
+		success: function(msg){
+			if(msg=='success'){
+				window.location.href='/thank.html';
+			}
+		}
+	});
+}
