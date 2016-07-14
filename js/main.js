a00a783933edd4921b01ec82ccfed115509069e1
Raju216 <raju.rai@paxcom.net>
2016-07-14 12:50:39 +0530

    edit

--- js/main.js
+++ js/main.js
@@ -224,9 +224,9 @@ $(document).ready(function () {
 
 
 $('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(200);
+  $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(0);
 }, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(200);
+  $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(0);
 });
 
 
