0ba0171b8bb54bd2ba22cafa01cf63dd11d39f66
Raju216 <raju.rai@paxcom.net>
2016-07-14 12:51:52 +0530

    edit

--- js/main.js
+++ js/main.js
@@ -223,11 +223,11 @@
 $(document).ready(function () {
 
 
-$('ul.nav li.dropdown').hover(function() {
+/*$('ul.nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(0);
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(0);
-});
+});*/
 
 
 /*Scroll Spy*/
